d44a596366a646f342e163dd43d2a84b52c8e295
SridharClover <gsridhar264@gmail.com>
2024-02-22 11:59:50 +0530

    structure changed

--- src/main/java/com/student/StudentApplication.java
+++ src/main/java/com/student/StudentApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class InstitutionApplication {
+public class StudentApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(InstitutionApplication.class, args);
+		SpringApplication.run(StudentApplication.class, args);
 	}
 
 }
